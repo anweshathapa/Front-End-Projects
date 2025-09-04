17fe36cc6710fd38234267a447f714ee6174f302
Anwesha Thapa <anweshathapa15@gmail.com>
2025-09-04 16:27:37 +0530

    JavaScript Code

--- kanban board /script.js
+++ kanban board /script.js
@@ -4,8 +4,8 @@ const cards = document.querySelectorAll(".card");
 const lists = document.querySelectorAll(".list");
 
 for(const card of cards){
-    card.addEventListener("dragstart", dragStart);
-    card.addEventListener("dragend", dragEnd);
+card.addEventListener("dragstart", dragStart);
+card.addEventListener("dragend", dragEnd);
 }
 for(const list of lists){
     list.addEventListener("dragover", dragOver);
